13d76db7850ebf79f6fe80829d230a511d863207
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-04-13 21:16:12 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,34 @@
 #include <string>
 
 bool collective_anagrams(std::string str_1, std::string str_2) {
-  return false;
+  int count;
+  if (str_1.length() != str_2.size()) {
+    return false;
+  } else {
+    for (int i = 0; i < str_1.size(); i++) {
+      for (int j = 0; j < str_1.size(); j++) {
+        if (str_1[i] == str_2[j]) {
+          count++;
+          break;
+        } else
+          return false;
+      }
+    }
+    if (count == str_1.size()) {
+      return true;
+    }
+  }
 }
 
 int main() {
   std::cout << "Name Project" << std::endl;
   std::string word_1;
   std::string word_2;
+  std::cout << "Enter the first word" << std::endl;
+  std::cin >> word_1;
+  std::cout << "Enter the second word" << std::endl;
+  std::cin >> word_2;
+
   if (collective_anagrams(word_1, word_2) == true) {
     std::cout << "These words are anagrams" << std::endl;
   } else {
