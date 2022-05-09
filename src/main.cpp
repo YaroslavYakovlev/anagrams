2e9b376e2cde5505662d4865c09b422c3d6c33bb
Yaroslav Yakovlev <ekb-buza@mail.ru>
2022-05-09 13:43:44 +0500

    решение

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,46 @@
 #include <map>
 #include <string>
 
-bool collective_anagrams(std::string str_1, std::string str_2) {
-  int count[256] = {0};
-  int i;
-  for (i = 0; str_1[i] && str_2[i]; i++) {
-    count[str_1[i]]++;
-    count[str_2[i]]--;
-  }
-  if (str_1[i] || str_2[i])
-    return false;
-  for (i = 0; i < 256; i++)
-    if (count[i])
+bool CollectiveAnagrams(std::map<char, int> word1, std::map<char, int> word2) {
+  for (std::map<char, int>::iterator it_1 = word1.begin(); it_1 != word1.end(); it_1++) {
+    if (word2.find(it_1->first) != word2.end()) {
+      return true;
+    } else
       return false;
-  return true;
+  }
+}
+
+std::map<char, int> BuildCharCounters(std::string& str) {
+  std::map<char, int> tmpMap;
+  for (const auto& ch : str) {
+    ++tmpMap[ch];
+  }
+  return tmpMap;
 }
 
 int main() {
   std::cout << "Anagrams" << std::endl;
+  std::map<char, int> word1;
+  std::map<char, int> word2;
   std::string word_1;
   std::string word_2;
   std::cout << "Enter the first word" << std::endl;
   std::cin >> word_1;
+  word1 = BuildCharCounters(word_1);
+
   std::cout << "Enter the second word" << std::endl;
   std::cin >> word_2;
+  word2 = BuildCharCounters(word_2);
 
-  if (collective_anagrams(word_1, word_2) == true) {
+  for (std::map<char, int>::iterator it = word1.begin(); it != word1.end(); it++) {
+    std::cout << "Word1 [key] - " << it->first << " value - " << it->second << std::endl;
+  }
+  std::cout << std::endl;
+  for (std::map<char, int>::iterator it = word2.begin(); it != word2.end(); it++) {
+    std::cout << "Word2 [key] - " << it->first << " value - " << it->second << std::endl;
+  }
+
+  if (CollectiveAnagrams(word1, word2) == true) {
     std::cout << "These words are anagrams" << std::endl;
   } else {
     std::cout << "These words are not anagrams" << std::endl;
